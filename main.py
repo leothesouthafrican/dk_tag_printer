7f9138155d89264c40d985a754dd96fb2e85d25e
leothesouthafrican <38779415+leothesouthafrican@users.noreply.github.com>
2022-09-29 18:50:49 +0100

    better borders, font size and max char

--- main.py
+++ main.py
@@ -4,13 +4,6 @@ import pandas as pd
 from fpdf import FPDF
 import base64
 
-#defining functions
-def show_pdf(file_path):
-    with open(file_path,"rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
 #setting page header
 st.header("DasKasas Tag Tool")
 
@@ -19,6 +12,8 @@ with st.sidebar:
     csv = st.file_uploader("Please Upload Updated CSV Export from DEAR Inventory.")
     tag_height = st.number_input("Tag Height (mm)", min_value=0, max_value=100, value=34)/3
     tag_width = st.number_input("Tag Width (mm)", min_value=0, max_value=100, value=60)
+    font_size = st.number_input("Font Size", min_value=8, max_value=100, value=8)
+    max_characters = st.number_input("Max Characters", min_value=37, max_value=60, value=37)
 
 #if a csv has been uploaded load the csv as a pandas df and display it for verification by user
 if csv:
@@ -40,7 +35,7 @@ if csv:
     df = df.loc[df["ProductCode"].isin(selected_products)]
     df = df.reset_index(drop=True)
     #Allow the description to only have a length of 40 characters
-    df["Name"] = df["Name"].str[:37]
+    df["Name"] = df["Name"].str[:max_characters]
     
     #displaying the new dataframe with only selected products for verification by user
     st.dataframe(df)
@@ -49,28 +44,14 @@ if csv:
     button = st.button("Process Tags")
 
     if button:
-
         # instantiation of inherited class 
         pdf = FPDF(unit= "mm", format = "A4")
 
         #creating a blank page and setting font
         pdf.add_page("L")
-        pdf.set_font('Arial', 'B', 8)
+        pdf.set_font('Arial', 'B', font_size)
 
-        #adding horizontal lines to page
-        pdf.line(pdf.l_margin - 2, 0, pdf.l_margin - 2, pdf.h)
-        pdf.line(tag_width + 8, 0, tag_width + 8, pdf.h)
-        pdf.line(tag_width *2 + 8, 0, tag_width * 2 + 8, pdf.h)
-        pdf.line(tag_width *3 + 8, 0, tag_width * 3 + 8, pdf.h)
-        pdf.line(tag_width *4 + 8, 0, tag_width * 4 + 8, pdf.h)
-
-        #adding vertical lines to pdf
-        pdf.line(0, tag_height, pdf.w, tag_height)
-        pdf.line(0, tag_height *4, pdf.w, tag_height * 4)
-        pdf.line(0, tag_height *7, pdf.w, tag_height * 7)
-        pdf.line(0, tag_height *10, pdf.w, tag_height * 10)
-        pdf.line(0, tag_height *13, pdf.w, tag_height * 13)
-        pdf.line(0, tag_height *16, pdf.w, tag_height * 16)
+        #Creating the cells
 
         #getting initial x coordinate
         x_initial = pdf.get_x()
@@ -84,6 +65,8 @@ if csv:
                 pdf.set_xy(pdf.get_x()-tag_width, pdf.get_y() + tag_height)
                 pdf.cell(w = tag_width, h = tag_height, txt = "R" + "{:0.2f}".format(df.at[index, price_chosen]*1.15), border = 0)
                 pdf.set_xy(pdf.get_x(),pdf.get_y()-2*tag_height)
+                #create border around all three cells
+                pdf.rect(pdf.get_x()-tag_width, pdf.get_y(), tag_width, 3*tag_height)
         
             else:
             #if not create new tags on new full tag line (tag_height * 3)
@@ -94,12 +77,11 @@ if csv:
                 pdf.set_xy(pdf.get_x()-tag_width, pdf.get_y() + tag_height)
                 pdf.cell(w = tag_width, h = tag_height, txt = "R" + "{:0.2f}".format(df.at[index, price_chosen]*1.15), border = 0)
                 pdf.set_xy(pdf.get_x(),pdf.get_y()-2*tag_height)
+                #create border around all three cells
+                pdf.rect(pdf.get_x()-tag_width, pdf.get_y(), tag_width, 3*tag_height)
     
         #store output pdf in memory
         final_pdf = pdf.output("test.pdf")
-
-        #display pdf in the app using show_pdf function
-        show_pdf("test.pdf")
 
         #reading the saved pdf file and storing it as PDFbyte
         with open("test.pdf", "rb") as pdf_file:
